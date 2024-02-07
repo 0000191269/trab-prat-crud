f97bcb5adf57c498d228d26bc5dbed8cb74627bd
prhz <139805456+prhz@users.noreply.github.com>
2024-02-06 23:40:05 -0300

    att de redirect e pagina unica de resultadso de pesquisa

--- trabprat/trabprat/src/main/java/com/example/trabprat/controller/IndexController.java
+++ trabprat/trabprat/src/main/java/com/example/trabprat/controller/IndexController.java
@@ -5,6 +5,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import com.example.trabprat.model.Funcionario;
 import com.example.trabprat.model.SearchId;
 import com.example.trabprat.model.Sistema;
@@ -13,18 +17,27 @@ import com.example.trabprat.model.Sistema;
 public class IndexController {
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index() {
         return "index";
     }
 
-    @PostMapping("/add")
-    public String userAdd(@ModelAttribute Funcionario f, Model model) {
-        Sistema.addFuncionario(f);
-        return "index";
+    @GetMapping("/addRedirect")
+    public String redirectToAdd() {
+        return "redirect:/add";
     }
 
-    @GetMapping("/buscar")
-    public String userSearch(@ModelAttribute SearchId id, Model model) {
+    @GetMapping("/add")
+    public String addPage() {
+        return "add";
+    }
+
+    @GetMapping("/searchRedirect")
+    public String redirectToSearch() {
+        return "redirect:/search";
+    }
+
+    @GetMapping("/result")
+    public String result(@ModelAttribute SearchId id, Model model) {
         Sistema.addFuncionario(new Funcionario("erick", "321", "gamil@email.com", 18));
         Sistema.addFuncionario(new Funcionario("pedro", "abc", "email@email.com", 18));
         Funcionario f2 = Sistema.getFuncionario(id.getId());
@@ -32,6 +45,11 @@ public class IndexController {
         model.addAttribute("idade", f2.getIdade());
         model.addAttribute("email", f2.getEmail());
         model.addAttribute("id", f2.getId());
-        return "buscar";
+        return "result";
+    }
+
+    @GetMapping("/search")
+    public String userSearch() {
+        return "search";
     }
 }
